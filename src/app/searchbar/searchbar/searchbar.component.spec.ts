dec95aa0878749362d42f0159b13de60cfd11b9e
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2026-03-31 15:25:59 -0500

    Tests unitarios para searchbar implementados

--- src/app/searchbar/searchbar/searchbar.component.spec.ts
+++ src/app/searchbar/searchbar/searchbar.component.spec.ts
@@ -1,9 +1,8 @@
 /* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SearchBarComponent } from './searchbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -12,7 +11,7 @@ describe('SearchBarComponent', () => {
   beforeEach(async() => {
     await TestBed.configureTestingModule({
       declarations: [SearchBarComponent],
-      imports: [ReactiveFormsModule]
+      imports: [ReactiveFormsModule, RouterTestingModule]
     }).compileComponents();
   });
 
@@ -24,5 +23,36 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create the form', () => {
+    expect(component.searchForm).toBeTruthy();
+  });
+
+  it('should be invalid when empty', () => {
+    expect(component.searchForm.valid).toBeFalsy();
+  });
+
+  it('should be valid when all fields are filled', () => {
+    component.searchForm.setValue({
+      ciudad: 'Bogota',
+      check_in: '2024-07-01',
+      check_out: '2024-07-10',
+      capacidad: 2
+    });
+    expect(component.searchForm.valid).toBeTruthy();
+  });
+
+  it('should disable button if form is invalid', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeTruthy();
+  });
+
+  it('should call buscar() on form submit', () => {
+    vi.spyOn(component, 'buscar');
+
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit'));
+    expect(component.buscar).toHaveBeenCalled();
   });
 });
