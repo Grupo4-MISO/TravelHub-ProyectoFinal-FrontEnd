918e3ca523d049574d562242bc762164d2c58075
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2026-04-10 14:48:06 -0500

    Añadimos reviews a cada hospedaje

--- src/app/searchbar/searchbar.ts
+++ src/app/searchbar/searchbar.ts
@@ -1,12 +1,15 @@
 export interface SearchBar {
   habitacion_id: string;
   hospedaje_id: string;
+  code: string;
   nombre: string;
   pais: string;
   ciudad: string;
   direccion: string;
   rating: number;
+  reviews: number;
   capacidad: number;
   precio: number;
   descripcion: string;
+  currency_code: string;
 }
