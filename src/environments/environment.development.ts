594776fd2f6e7a3125b64999ad74c2cab5777830
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2026-04-26 17:31:45 -0500

    Actualizamos package.json

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    inventariosUrl: 'http://127.0.0.1:3000',
-    reservasUrl: 'http://localhost:3001',
-    searchUrl: 'http://localhost:3002',
-    comentariosUrl: 'http://localhost:3003',
-    authUrl: 'http://localhost:3004',
-    proveedores: "http://localhost:3005",
-    trasacciones: "http://127.0.0.1:3006",
-    clientes: "http://localhost:3007",
+    inventariosUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
+    reservasUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
+    searchUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
+    comentariosUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
+    authUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
+    proveedores: "https://dpyrs6tuvj15e.cloudfront.net",
+    trasacciones: "https://dpyrs6tuvj15e.cloudfront.net",
+    clientes: "https://dpyrs6tuvj15e.cloudfront.net",
     apiUrl: 'https://dpyrs6tuvj15e.cloudfront.net'
 };
