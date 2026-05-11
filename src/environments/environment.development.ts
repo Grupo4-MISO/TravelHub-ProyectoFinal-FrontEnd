f858cc69bfa7862dffeb56fd9eb667bdd710fc63
Juan_Camilo <jc.morag12@uniandes.edu.co>
2026-05-10 19:13:54 -0500

    Se agregan las funcionalidades correspondientes a la busqueda y filtro de reservas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    inventariosUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
-    reservasUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
-    searchUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
-    comentariosUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
-    authUrl: 'https://dpyrs6tuvj15e.cloudfront.net',
-    proveedores: "https://dpyrs6tuvj15e.cloudfront.net",
-    trasacciones: "https://dpyrs6tuvj15e.cloudfront.net",
-    clientes: "https://dpyrs6tuvj15e.cloudfront.net",
-    apiUrl: 'https://dpyrs6tuvj15e.cloudfront.net'
+    inventariosUrl: 'http://localhost:3000',
+    reservasUrl: 'http://localhost:3001',
+    searchUrl: 'http://localhost:3002',
+    comentariosUrl: 'http://localhost:3003',
+    authUrl: 'http://localhost:3004',
+    proveedores: "http://localhost:3006",
+    trasacciones: "http://localhost:3005",
+    clientes: "http://localhost:3007",
+    apiUrl: 'http://localhost:3008'
 };
