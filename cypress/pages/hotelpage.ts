6a47e2a3e75bb69c465255fc604298437a036d3f
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2026-05-16 10:42:24 -0500

    Avances e2e

--- cypress/pages/hotelpage.ts
+++ cypress/pages/hotelpage.ts
@@ -2,4 +2,8 @@ export class HotelPage {
     static getReservasManager() {
         return cy.get('[data-cy="reservas-header"]').should('be.visible');
     }
+
+    static clickBuscarReservas(){
+        return cy.get('[data-cy="canvas-buscar"]').click({ force: true});
+    }
 }
